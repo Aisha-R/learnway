57f5d8ea635a39b96cee2d0b9dfdccc469649172
Aisha-R <aish0695@stud.kea.dk>
2023-08-28 17:55:22 +0200

    added authtokens app and set up JWT

--- urls.py
+++ urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls')),
+    path('auth/', include('authtokens.urls', namespace='authtokens')),
     path('items/', include('items.urls', namespace='items'))
 ]
